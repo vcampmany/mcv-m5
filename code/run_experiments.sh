f300145724bfec3a3976e449c314d1ae4e4d1b1e
Guillem Cucurull <gcucurull@gmail.com>
2017-03-14 10:53:54 +0100

    wrn dataug

--- code/run_experiments.sh
+++ code/run_experiments.sh
@@ -10,8 +10,8 @@
 
 #CUDA_VISIBLE_DEVICES=0 python train.py -c config/test3.py -e norm_imagenet -l /home/master/experiments -s /home/master/experiments
 
-CUDA_VISIBLE_DEVICES=0 python train.py -c config/kitti_scratch.py -e kitti_scratch -l /home/master/experiments -s /home/master/experiments
+CUDA_VISIBLE_DEVICES=0 python train.py -c config/dataug_wrn.py -e dataug_wrn -l /home/master/experiments -s /home/master/experiments
 
-sleep 5
+#sleep 5
 
-CUDA_VISIBLE_DEVICES=0 python train.py -c config/kitti_imagenet.py -e kitti_imagenet -l /home/master/experiments -s /home/master/experiments
+#CUDA_VISIBLE_DEVICES=0 python train.py -c config/kitti_imagenet.py -e kitti_imagenet -l /home/master/experiments -s /home/master/experiments
